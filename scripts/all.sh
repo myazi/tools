e6c9bc8ed06d58a75a6e4d082de176adef5a8421
myazi <869233039@qq.com>
2022-11-07 20:07:38 +0800

    scripts

--- scripts/all.sh
+++ scripts/all.sh
@@ -5,54 +5,155 @@
 # Created Time: Wed 27 May 2020 12:05:12 PM CST
 #########################################################################
 #!/bin/bash
-source ~/.bashrc
+source ~/.bashrc #文件包含进来
 source ~/.bash_profile
 BIN='./bin'
 DATA_DIR='./data'
 RESULT_DIR='./data/result'
 GRAHP_TRY_DAY=10
 
-# todo 单进程运行，后续要删除
+#变量
+#获取字符串长度
+string="abcd"
+echo ${#string[0]}   # 输出 4
+#提取子字符串
+string="runoob is a great site"
+echo ${string:1:4} # 输出 unoo
+#查找子字符串
+string="runoob is a great site"
+echo `expr index "$string" io`  # 输出 4
+
+#数组
+array_name=(value0 value1 value2 value3) #定义
+# 取得数组元素的个数
+length=${#array_name[@]}
+# 或者
+length=${#array_name[*]}
+# 取得数组单个元素的长度
+lengthn=${#array_name[n]}
+echo "数组的元素为: ${array_name[*]}"
+
+#参数传递
+echo "Shell 传递参数实例i"
+echo "第一个参数为：$1"
+
+echo "参数个数为：$#"
+echo "传递的参数作为一个字符串显示：$*"
+echo "传递的参数作为一个字符串显示：$@"
+
+#运算
+val=`expr 2 + 2`
+echo "两数之和为 : $val"
+
+# -eq, -nq, -ge, -le, -gt, -lt, -a[&&], -o[||], !
+if [ $a != $b ]
+then
+    echo "$a != $b : a 不等于 b"
+else
+    echo "$a == $b: a 等于 b"
+fi
+if [ $a -lt 100 -a $b -gt 15 ]
+then
+    echo "$a 小于 100 且 $b 大于 15 : 返回 true"
+else
+    echo "$a 小于 100 且 $b 大于 15 : 返回 false"
+fi
+if [ $a -lt 100 -o $b -gt 100 ]
+then
+    echo "$a 小于 100 或 $b 大于 100 : 返回 true"
+else
+    echo "$a 小于 100 或 $b 大于 100 : 返回 false"
+fi
+if [ $a -lt 5 -o $b -gt 100 ]
+then
+    echo "$a 小于 5 或 $b 大于 100 : 返回 true"
+else
+    echo "$a 小于 5 或 $b 大于 100 : 返回 false"
+fi
+if [ -z $a ]
+then
+    echo "-z $a : 字符串长度为 0"
+else
+    echo "-z $a : 字符串长度不为 0"
+fi
+if [ -n "$a" ]
+then
+    echo "-n $a : 字符串长度不为 0"
+else
+    echo "-n $a : 字符串长度为 0"
+fi
+if [ $a ]
+then
+    echo "$a : 字符串不为空"
+else
+    echo "$a : 字符串为空"
+fi
+
+#函数
+funWithParam(){
+    echo "第一个参数为 $1 !"
+    echo "第十个参数为 ${10} !"
+    echo "参数总数有 $# 个!"
+    echo "作为一个字符串输出所有参数 $* !"
+}
+if [ $? -eq 0 ]; then
+    echo "上一条命令wget hadoop执行成功"
+else
+    echo "上一条命令wget hadoop执行失败"
+do
+
+#输出
+printf "%-10s %-8d %c-1c %-4.2f\n" name 100 g 48.6543 ##字符串、整型、字符、小数
+command > file 2>&1 ## 1标准输出、2标准错误输出，将1，2都重定向到file
+command >> file 2>&1 ## 1标准输出、2标准错误输出，将1，2都重定向追加到file
+
+# 文件/目录相关
+if [ ! -d ./data ]; then
+	mkdir ./data
+fi
+if [ ! -f ${DATA_DIR}/${day} ]; then
+    echo "miss rdc_file ${day}"
+    exit -1
+fi
+if [ -s ${RESULT_DIR}/res_${day} ]; then
+    echo "${day} remain again"
+    continue
+fi
+
+#循环操作
+for file in `ls /etc`; do
+done
+
+## time相关
+day=`date -d "-${j} days" +%Y%m%d`
+one_day=`date -d "$day 00:00:00" +%s`
+cur_day=`date -d "-${i} days" +%Y%m%d`
+graph_date=${tm:0:8}
+
+# 单进程运行
 IP_FILE='run_daily.pid'
 if [ -f ./$IP_FILE ];then
     date +%Y-%m-%d:%H-%M-%S
     echo "gicf not over" | mutt yingwenjie@baidu.com -s "last not finish"
     exit
 fi
-
-if [ ! -d ./data ]; then
-	mkdir ./data
-fi
+echo "${day} PID of this script: $$" > ./$IP_FILE"
 
 for((i=1;i>0;i--));
 do
-    echo $day "+" $one_day
     j=`expr $i + 1`
-    day=`date -d "-${j} days" +%Y%m%d`
-    one_day=`date -d "$day 00:00:00" +%s`
-    cur_day=`date -d "-${i} days" +%Y%m%d`
-    graph_date=${tm:0:8}
-    if [ -s ${RESULT_DIR}/res_${day} ]; then
-        echo "${day} remain again"
-        continue
-    fi
-    wget -P ${DATA_DIR} ftp://local_ec:local_ec@xxx//home/disk1/yangjintai/workspace/jobs/local_video_report/result/${cur_day}/active_${cur_day}
-    if [ ! -f ${DATA_DIR}/${day} ]; then
-        echo "miss rdc_file ${day}"
-        exit -1
-    fi
 
-    cat ${DATA_DIR}/active_${day} ${DATA_DIR}/active_${cur_day} > ${DATA_DIR}/${day}_active_tmp
+    wget -P ${DATA_DIR} ftp://local_ec:local_ec@xxx//home/disk1/yangjintai/workspace/jobs/local_video_report/result/${cur_day}/active_${cur_day}
 
     hadoop fs -D hadoop.job.ugi=xxx -D fs.default.name=xx -getmerge  afs://yinglong.afs.baidu.com:9902/user/feed_rcmc/job_data/production/rcmc_data/${cur_day}/00/local_ec_nid_set_video z_${cur_day}_00.gz
 
     /opt/compiler/gcc-8.2/lib/ld-linux-x86-64.so.2 --library-path /opt/compiler/gcc-8.2/lib `which python` ${BIN}/get_gender_cate_tags.py ${DATA_DIR}/nid_file_${cur_day} > ${DATA_DIR}/nid_dict_${cur_day}
 done
-#备份&&删除无用文件
+
+#删除无用文件
 DATE_LOG=`date -d " $DATE 1 hays ago" +%Y%m%d`
 rm ${DATA_DIR}/$DATE_LOG
 rm ${DATA_DIR}/click_*$DATE_LOG*
-
 
 cluster_num=(8 10 12 16 24)
 cluster_files=(xx_fangdichan_query xx_huahzuang_query xx_jiaju_query xx_jiankangyangsheng_query xx_jiaoyu_query xx_lvyou_query xx_meishi_query xx_qiche_query xx_shuma_query)
@@ -70,10 +171,9 @@ do
     done
 done
 
-    ###读数据库获取历史数据
-    mysql -u root -p123456 -D yongshangyiti -e "select mthid, author from all_nid_details" > ${DATA_DIR}/history_mthid_details;
-#sort -t $'\t' -k 3n ./data/random_res_kmeans_qinggan > ./data/sort_random_res_kmeans_qinggan
-    mysql --local-infile -uroot -p123456 yongshangyiti -e "LOAD DATA LOCAL INFILE  '${RESULT_DIR}/ertiao_nid_${day}'  REPLACE INTO TABLE microv_nid_info  FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n' (event_day,nid,author,title,duration,public_time,click,short,finish_prob,finish_rate,zhuanhua,zhuanhua_show,dur_join_scc,like_v,follow,share,comment_view,dislike,active_join_scc,mthid,convert_type)"
+# mysql数据库操作
+mysql -u root -p123456 -D yongshangyiti -e "select mthid, author from all_nid_details" > ${DATA_DIR}/history_mthid_details;
+mysql --local-infile -uroot -p123456 yongshangyiti -e "LOAD DATA LOCAL INFILE  '${RESULT_DIR}/ertiao_nid_${day}'  REPLACE INTO TABLE microv_nid_info  FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n' (event_day,nid,author,title,duration,public_time,click,short,finish_prob,finish_rate,zhuanhua,zhuanhua_show,dur_join_scc,like_v,follow,share,comment_view,dislike,active_join_scc,mthid,convert_type)"
 
 # 遍历视频队列，获取最终结果
 for key in local_ec_video kankan_video
@@ -86,7 +186,9 @@ do
     
     fi
 } &
+
 md5sum ./data/${dict} > ./data/${dict}.md5 
+
 #!/bin/sh
 
 LOG() {
